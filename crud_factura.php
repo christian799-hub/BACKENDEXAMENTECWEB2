f4ae2f7d28e07c5631718decb5980ef9536c123d
Christian Ferrufino <christian.ferrufino.m@ucb.edu.bo>
2026-06-12 00:20:07 -0400

    Update crud_factura.php

--- crud_factura.php
+++ crud_factura.php
@@ -3,7 +3,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-header("Access-Control-Allow-Origin: *");
+$allowed_origin = "https://examen-final-christian-ferrufino.vercel.app";
+header("Access-Control-Allow-Origin: $allowed_origin");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, Accept, body");
 header("Content-Type: application/json; charset=UTF-8");
