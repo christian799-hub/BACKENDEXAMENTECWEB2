475c18bba691725593e02ade8fe39500d56cb663
Christian Ferrufino <christian.ferrufino.m@ucb.edu.bo>
2026-06-12 00:20:22 -0400

    Update get_facturas.php

--- get_facturas.php
+++ get_facturas.php
@@ -1,6 +1,6 @@
 <?php
-// api/get_facturas.php
-header("Access-Control-Allow-Origin: *");
+$allowed_origin = "https://examen-final-christian-ferrufino.vercel.app";
+header("Access-Control-Allow-Origin: $allowed_origin");
 header("Content-Type: application/json; charset=UTF-8");
 
 // Configuración de la base de datos
